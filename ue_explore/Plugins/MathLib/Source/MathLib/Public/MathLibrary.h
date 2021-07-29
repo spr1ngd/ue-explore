9c1f1be38d13c40701e4fc0d8bd211bd8bd78e5a
springdong <springu3d@yeah.net>
2021-07-29 22:59:51 +0800

    add new algorithm : get convex points of a set of coplanar points in arbitrary 3d direciton.

--- ue_explore/Plugins/MathLib/Source/MathLib/Public/MathLibrary.h
+++ ue_explore/Plugins/MathLib/Source/MathLib/Public/MathLibrary.h
@@ -7,6 +7,13 @@ namespace S
 	class MATHLIB_API FMathLibrary 
 	{
 	public:
-		static void Convex(TArray<FVector> points,FVector pn);
+		static void Convex(TArray<FVector>& points,FVector pN,TArray<FVector>& convexPoints);
+		static FMatrix MatrixFromAToB(FVector A,FVector B);
+		static float DistanceToVector(FVector p,FVector o,FVector dir);
+
+	private:
+		static void Convex_Internal(
+			FVector stdN, FVector begin, FVector end, TArray<FVector> points,
+			TArray<FVector>& convexPoints, int32 rightSlotIndex, int32 leftSlotIndex);
 	};
 }
